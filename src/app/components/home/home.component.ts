55ab58a1812796707426fa8c27e2b9a863bd0ca2
Christian von Koch <christian.von.koch@gmail.com>
2019-04-09 18:10:59 +0200

    New try with app, better upload

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  user;
 
   constructor(public auth: AuthService) {
     this.auth.user$.subscribe(user => this.user = user)
