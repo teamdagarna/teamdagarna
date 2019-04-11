017da385820365f469092133660749486a8a632f
Christian von Koch <christian.von.koch@gmail.com>
2019-04-11 15:36:12 +0200

    Navbar now dissapears on click when on mobile

--- src/assets/js/bulma-responsive.js
+++ src/assets/js/bulma-responsive.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
+  var $navItems = Array.prototype.slice.call(document.querySelectorAll('.mobileLink'), 0);
+
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
 
@@ -20,6 +22,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
       });
     });
+  }
+  // Check if there are any navbar items with class .mobileLink
+  if($navItems.length > 0) {
+
+    // Add a click event on each of them
+    $navItems.forEach(function ($e1) {
+      $e1.addEventListener('click', function() {
+        // Get the target from the "data-target" attribute
+        var target = "navbarExampleTransparentExample"
+        var $target = document.getElementById(target);
+
+        var target2 = "navbarBurger"
+        var $target2 = document.getElementById(target2);
+
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $target.classList.toggle('is-active');
+        $target2.classList.toggle('is-active');
+      })
+    })
   }
 
 });
