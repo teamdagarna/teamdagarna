5c522b986fe838115c05ad7f1471dc227d7a3a63
Christian von Koch <christian.von.koch@gmail.com>
2019-05-05 17:22:52 +0200

    New try with app

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -376,6 +376,100 @@ export class AuthService {
         var json = JSON.stringify(items);
 
         window.location.href = 'gonative://sidebar/setItems?items=' + encodeURIComponent(json);
+    } else {
+        var items = [{
+          "subLinks": [],
+          "label": "Hem",
+          "url": "http://teamdagarna.com/",
+          "icon": "fa-home"
+        },
+        {
+          "subLinks": [
+            {
+              "subLinks": [],
+              "label": "Sök DreamTEAM",
+              "url": "http://teamdagarna.com/sokdreamteam"
+            },
+            {
+              "subLinks": [],
+              "label": "Företagen 2018",
+              "url": "http://teamdagarna.com/companies"
+            },
+            {
+              "subLinks": [],
+              "label": "Sök kontaktsamtal",
+              "url": "https://teamdagarna.com/interview"
+            },
+            {
+              "subLinks": [],
+              "label": "Event",
+              "url": "https://teamdagarna.com/events"
+            }
+          ],
+          "label": "För Studenter",
+          "isGrouping": true,
+          "icon": "fa-user"
+        },
+        {
+          "subLinks": [
+            {
+              "subLinks": [],
+              "label": "Om dagarna",
+              "url": "http://teamdagarna.com/about"
+            },
+            {
+              "subLinks": [],
+              "label": "FAQ",
+              "url": "https://teamdagarna.com/faq"
+            },
+            {
+              "subLinks": [],
+              "label": "Erbjudande",
+              "url": "https://teamdagarna.com/weoffer"
+            },
+            {
+              "subLinks": [],
+              "label": "Hitta hit",
+              "url": "https://teamdagarna.com/weoffer"
+            }
+          ],
+          "label": "För företag",
+          "isGrouping": true,
+          "icon": "fa-briefcase"
+        },
+        {
+          "subLinks": [
+            {
+              "subLinks": [],
+              "label": "Kontakt",
+              "url": "https://teamdagarna.com/dreamteam"
+            },
+            {
+              "subLinks": [],
+              "label": "Om dagarna",
+              "url": "https://teamdagarna.com/dreamteam"
+            }
+          ],
+          "label": "Om oss",
+          "isGrouping": true,
+          "icon": "fa-question"
+        },
+        {
+          "subLinks": [],
+          "label": "English info",
+          "url": "https://teamdagarna.com/about-english",
+          "icon": "fa-cog"
+        },
+        {
+          "subLinks": [],
+          "label": "Logga in",
+          "url": "https://teamdagarna.com/signin",
+          "icon": "fa-sign-out"
+        }
+      ];
+        var json = JSON.stringify(items);
+
+        window.location.href = 'gonative://sidebar/setItems?items=' + encodeURIComponent(json);
       }
     }
     const allowed = ['dreamteamPlatinumMember', 'dreamteamMember']
