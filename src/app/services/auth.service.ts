49554ec602a81b707ad752e09f49d6e2115a61f2
Christian von Koch <christian.von.koch@gmail.com>
2019-05-28 20:40:18 +0200

    Test dreamteamadmin with app

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -33,7 +33,10 @@ export class AuthService {
                     if (user) {
                       //Checks if request comes from the app
                       if (navigator.userAgent.indexOf('gonative') > -1) {
+                        var currentUser: any;
+                        currentUser = firebase.auth().currentUser;
                         //Checks if user is logged in and presents the content in the navbar differently depending on the answer
+                        if (currentUser.roles['dreamteamMember']) {
                           var items = [{
                             "subLinks": [],
                             "label": "Hem",
@@ -128,13 +131,121 @@ export class AuthService {
                             "label": "Logga ut",
                             "url": "https://teamdagarna.com/signout",
                             "icon": "fa-sign-out"
+                          },
+                          {
+                            "subLinks": [],
+                            "label": "DreamTEAM Admin",
+                            "url": "https://teamdagarna.com/dreamteamadmin",
+                            "icon": "fa-star"
                           }
                         ];
 
                         var json = JSON.stringify(items);
 
                         window.location.href = 'gonative://sidebar/setItems?items=' + encodeURIComponent(json);
-                      }
+                      } else {
+                        var items = [{
+                          "subLinks": [],
+                          "label": "Hem",
+                          "url": "http://teamdagarna.com/",
+                          "icon": "fa-home"
+                        },
+                        {
+                          "subLinks": [
+                            {
+                              "subLinks": [],
+                              "label": "Sök DreamTEAM",
+                              "url": "http://teamdagarna.com/sokdreamteam"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Företagen 2018",
+                              "url": "http://teamdagarna.com/companies"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Sök kontaktsamtal",
+                              "url": "https://teamdagarna.com/interview"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Event",
+                              "url": "https://teamdagarna.com/events"
+                            }
+                          ],
+                          "label": "För Studenter",
+                          "isGrouping": true,
+                          "icon": "fa-user"
+                        },
+                        {
+                          "subLinks": [
+                            {
+                              "subLinks": [],
+                              "label": "Om dagarna",
+                              "url": "http://teamdagarna.com/about"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "FAQ",
+                              "url": "https://teamdagarna.com/faq"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Erbjudande",
+                              "url": "https://teamdagarna.com/weoffer"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Hitta hit",
+                              "url": "https://teamdagarna.com/find"
+                            }
+                          ],
+                          "label": "För företag",
+                          "isGrouping": true,
+                          "icon": "fa-briefcase"
+                        },
+                        {
+                          "subLinks": [
+                            {
+                              "subLinks": [],
+                              "label": "Kontakt",
+                              "url": "https://teamdagarna.com/dreamteam"
+                            },
+                            {
+                              "subLinks": [],
+                              "label": "Om dagarna",
+                              "url": "https://teamdagarna.com/dreamteam"
+                            }
+                          ],
+                          "label": "Om oss",
+                          "isGrouping": true,
+                          "icon": "fa-question"
+                        },
+                        {
+                          "subLinks": [],
+                          "label": "English info",
+                          "url": "https://teamdagarna.com/about-english",
+                          "icon": "fa-cog"
+                        },
+                        {
+                          "subLinks": [],
+                          "label": "Din profil",
+                          "url": "https://teamdagarna.com/profile",
+                          "icon": "fa-user-graduate"
+                        },
+                        {
+                          "subLinks": [],
+                          "label": "Logga ut",
+                          "url": "https://teamdagarna.com/signout",
+                          "icon": "fa-sign-out"
+                        }
+                      ];
+
+                      var json = JSON.stringify(items);
+
+                      window.location.href = 'gonative://sidebar/setItems?items=' + encodeURIComponent(json);
+                    }
+                  }
                       this.isAuthenticated = true;
                       this.emailIsVerified = user.emailVerified;
                       return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
