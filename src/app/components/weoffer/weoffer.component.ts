7ec8046ba884eff8d377a485272078936d072979
Frida Andersson <frian632@student.liu.se>
2022-04-17 19:18:11 +0200

    Ändrat footer + olika sidor

--- src/app/components/weoffer/weoffer.component.ts
+++ src/app/components/weoffer/weoffer.component.ts
@@ -11,5 +11,19 @@ export class WeofferComponent implements OnInit {
 
   ngOnInit() {
   }
+   
+  openTab(event: any, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("content-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " is-active";
+  }
 
 }
