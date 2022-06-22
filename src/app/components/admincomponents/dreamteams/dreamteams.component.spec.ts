7100401375218e57856d8b0806691c590efbbe80
Frida Andersson <frian632@student.liu.se>
2022-06-22 13:18:00 +0200

    Design

--- src/app/components/admincomponents/dreamteams/dreamteams.component.spec.ts
+++ src/app/components/admincomponents/dreamteams/dreamteams.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DreamteamsComponent } from './dreamteams.component';
 
@@ -6,7 +6,7 @@ describe('DreamteamsComponent', () => {
   let component: DreamteamsComponent;
   let fixture: ComponentFixture<DreamteamsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DreamteamsComponent ]
     })
