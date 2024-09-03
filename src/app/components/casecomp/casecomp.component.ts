51496081e746bc0a36e81d609e49a361af09c7ce
frein784 <frein784@student.liu.se>
2024-09-03 15:19:28 +0200

    Made casecomppetition visible

--- src/app/components/casecomp/casecomp.component.ts
+++ src/app/components/casecomp/casecomp.component.ts
@@ -10,16 +10,31 @@ import { Router } from '@angular/router';
 export class CasecompComponent implements OnInit {
 
   user: any;
+  isButtonVisible: boolean = false;
 
   constructor(public auth: AuthService, private router: Router) {
     this.auth.user$.subscribe(user => this.user = user);
   }
 
   ngOnInit(): void {
+    this.checkTime();
+  }
+
+  checkTime() {
+    const now = new Date();
+    const targetDate = new Date(now.getFullYear(), 8, 4, 12, 0, 0, 0); // Set to 12:00 PM on September 4th (month is 0-indexed)
+
+    if (now >= targetDate) {
+      this.isButtonVisible = true;
+    } else {
+      const timeUntilVisible = targetDate.getTime() - now.getTime();
+      setTimeout(() => {
+        this.isButtonVisible = true;
+      }, timeUntilVisible);
+    }
   }
 
   redirectToLogin(): void {
     this.router.navigate(['/signin']); 
   }
-
 }
