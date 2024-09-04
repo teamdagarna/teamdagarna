cd1e24212539c5f2ac2706086e6db628e2c2f527
frein784 <frein784@student.liu.se>
2024-09-04 19:35:03 +0200

    new info for "kontaktsamtal"

--- src/app/components/application/application.component.ts
+++ src/app/components/application/application.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 declare var Jexpo: any;
 declare global {
@@ -12,15 +12,36 @@ declare global {
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.scss']
 })
-export class ApplicationComponent implements AfterViewInit {
+export class ApplicationComponent implements AfterViewInit, OnInit {
+
+  isMessageVisible: boolean = false;
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.checkTime();
+  }
 
   ngAfterViewInit(): void {
     window.Jexpo = (window.Jexpo || []).concat(function(){
       Jexpo.widget('/team/bundles/interviews-application.jsx', '#interviews-application');
     });
   }
+
+  checkTime() {
+    const now = new Date();
+    const targetDate = new Date(now.getFullYear(), 8, 5, 12, 0, 0, 0); // Set to 8:00 AM on September 5th (month is 0-indexed)
+
+    if (now >= targetDate) {
+      this.isMessageVisible = true;
+    } else {
+      const timeUntilVisible = targetDate.getTime() - now.getTime();
+      setTimeout(() => {
+        this.isMessageVisible = true;
+      }, timeUntilVisible);
+    }
+  }
+
   loadScript(url: string) {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
@@ -30,5 +51,4 @@ export class ApplicationComponent implements AfterViewInit {
       document.body.appendChild(script);
     });
   }
-
 }
