903a3ae9b5d61520fec2fdecf6ef63822a1340ea
Anja <anjsa296@student.liu.se>
2025-08-24 10:24:15 +0200

    Deleted old components not used anymore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,10 +39,6 @@ import { ForgotComponent } from './components/forgot/forgot.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/admincomponents/dashboard/dashboard.component';
 import { UserComponent } from './components/admincomponents/user/user.component';
-import { CompaniesComponent } from './components/companies/companies.component';
-import { AdmincompaniesComponent } from './components/admincomponents/admincompanies/admincompanies.component';
-import { CompanyComponent } from './components/admincomponents/company/company.component';
-import { AddcompanyComponent } from './components/admincomponents/addcompany/addcompany.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { AddstaffComponent } from './components/admincomponents/addstaff/addstaff.component';
 import { FilterPipe } from './shared/filter.pipe';
@@ -53,11 +49,7 @@ import { EducationsComponent } from './components/educations/educations.componen
 import { FaqComponent } from './components/faq/faq.component';
 import { WeofferComponent } from './components/weoffer/weoffer.component';
 import { VolunteerComponent } from './components/volunteer/volunteer.component';
-import { InterviewComponent } from './components/interview/interview.component';
 import { AddeventComponent } from './components/admincomponents/addevent/addevent.component';
-import { AddblogpostsComponent } from './components/admincomponents/addblogposts/addblogposts.component';
-import { BlogpostsComponent } from './components/blogposts/blogposts.component';
-import { InterviewsComponent } from './components/admincomponents/interviews/interviews.component';
 import { ProductportfolioComponent } from './components/productportfolio/productportfolio.component';
 import { JobOpportunitiesComponent } from './components/jobopportunities/jobopportunities.component';
 import { GoldpiecesComponent } from './components/goldpieces/goldpieces.component';
@@ -76,24 +68,16 @@ import { DatePipe } from '@angular/common';
 
 import { QuillModule } from 'ngx-quill';
 import { CompanysigninComponent } from './components/companycomponents/companysignin/companysignin.component';
-import { CompanycodesComponent } from './components/admincomponents/companycodes/companycodes.component';
-import { SelectComponent } from './components/companycomponents/select/select.component';
-import { CodeComponent } from './components/companycomponents/code/code.component';
 import { CheckinComponent } from './components/admincomponents/checkin/checkin.component';
 import { CompanymapComponent } from './components/companymap/companymap.component';
-import { AllinterviewsComponent } from './components/admincomponents/allinterviews/allinterviews.component';
 import { DreamteamsComponent } from './components/admincomponents/dreamteams/dreamteams.component';
-import { BlogpostComponent } from './components/admincomponents/blogpost/blogpost.component';
 import { MapComponent } from './components/map/map.component';
-import { InterviewscheduleComponent } from './components/admincomponents/interviewschedule/interviewschedule.component';
 import { ApplyComponent } from './components/apply/apply.component';
 import { SignoutComponent } from './components/signout/signout.component';
 import { SustainabilityComponent } from './components/sustainability/sustainability.component';
 import { SponsorComponent } from './components/sponsors/sponsors.component';
 import { FavouritecompaniesComponent } from './components/favouritecompanies/favouritecompanies.component';
 import { PrivacypolicyComponent } from './components/privacypolicy/privacypolicy.component';
-import { TreasurecodesComponent } from './components/admincomponents/treasurecodes/treasurecodes.component';
-import { TreasurescoreComponent } from './components/admincomponents/treasurescore/treasurescore.component';
 import { AppcompetitionComponent } from './components/appcompetition/appcompetition.component';
 import { CompanysignsComponent } from './components/companysigns/companysigns.component';
 import { LeaderboardComponent} from './components/leaderboard/leaderboard.component';
@@ -134,10 +118,6 @@ registerLocaleData(localeSv, 'sv');
     DashboardComponent,
     SponsorComponent,
     UserComponent,
-    CompaniesComponent,
-    AdmincompaniesComponent,
-    CompanyComponent,
-    AddcompanyComponent,
     SpinnerComponent,
     AddstaffComponent,
     FilterPipe,
@@ -148,32 +128,20 @@ registerLocaleData(localeSv, 'sv');
     FaqComponent,
     WeofferComponent,
     VolunteerComponent,
-    InterviewComponent,
     AddeventComponent,
-    AddblogpostsComponent,
-    BlogpostsComponent,
-    InterviewsComponent,
     EventsComponent,
     AdmineventsComponent,
     AdmineventComponent,
     CompanysigninComponent,
-    CompanycodesComponent,
-    SelectComponent,
-    CodeComponent,
     CheckinComponent,
     CompanymapComponent,
-    AllinterviewsComponent,
     DreamteamsComponent,
-    BlogpostComponent,
     MapComponent,
-    InterviewscheduleComponent,
     ApplyComponent,
     SignoutComponent,
     SustainabilityComponent,
     FavouritecompaniesComponent,
     PrivacypolicyComponent,
-    TreasurecodesComponent,
-    TreasurescoreComponent,
     AppcompetitionComponent,
     CompanysignsComponent,
     LeaderboardComponent,
@@ -215,23 +183,12 @@ registerLocaleData(localeSv, 'sv');
     children: [
         { path: '', component: DashboardComponent },
         { path: 'modifyuser/:id', component: UserComponent, canActivate: [PlatinumadminGuard] },
-        { path: 'companies', component: AdmincompaniesComponent },
-        { path: 'modifycompany/:id', component: CompanyComponent },
-        { path: 'addcompany', component: AddcompanyComponent },
         { path: 'addstaff', component: AddstaffComponent },
         { path: 'addevent', component: AddeventComponent },
-        { path: 'addblogposts', component: AddblogpostsComponent },
-        { path: 'interviews', component: InterviewsComponent, canActivate: [PlatinumadminGuard] },
         { path: 'events/:id', component: AdmineventComponent },
         { path: 'events', component: AdmineventsComponent },
-        { path: 'companycodes', component: CompanycodesComponent, canActivate: [PlatinumadminGuard] },
-        { path: 'treasurecodes', component: TreasurecodesComponent, canActivate: [PlatinumadminGuard] },
-        { path: 'allinterviews', component: AllinterviewsComponent, canActivate: [PlatinumadminGuard] },
-        { path: 'interviewschedule', component: InterviewscheduleComponent, canActivate: [PlatinumadminGuard] },
         { path: 'checkin', component: CheckinComponent },
         { path: 'dreamteam', component: DreamteamsComponent },
-        { path: 'blogpost', component: BlogpostComponent },
-        { path: 'treasurescore', component: TreasurescoreComponent }
       ] },
     { path: 'dreamteam', component: TeamComponent },
     { path: 'profile', component: ProfileComponent },
@@ -249,18 +206,14 @@ registerLocaleData(localeSv, 'sv');
     { path: 'weoffer', component: WeofferComponent },
     //{ path: 'sokvard', component: VolunteerComponent },
     { path: 'sokdreamteam', component: ApplyComponent },
-    //{ path: 'interview', component: InterviewComponent },
     { path: 'events', component: EventsComponent },
-    { path: 'blogposts', component: BlogpostsComponent },
     { path: 'fairmap', component: CompanymapComponent },
     { path: 'companysignin', component: CompanysigninComponent },
     { path: 'pre-registration', component: ExhibitorsPreRegistrationComponent},
     { path: 'hallbarhet', component: SustainabilityComponent},
     { path: 'integritetspolicy', component: PrivacypolicyComponent},
     { path: 'find', component: MapComponent },
-    //{ path: 'favoritforetag', component: FavouritecompaniesComponent},
     { path: 'foretagsskyltar', component: CompanysignsComponent},
-    { path: 'companyguest', component: SelectComponent, canActivate: [CompanyGuard] },
     { path: 'leaderboard', component: LeaderboardComponent},
     { path: 'quiz', component: QuizComponent},
     { path: 'vr', component: VRComponent},
