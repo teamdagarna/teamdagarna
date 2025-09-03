401369b3ec18772532c746f400c757ddcfddb285
Anja <anja.01.sandgren@gmail.com>
2025-09-03 10:45:55 +0200

    Closed applikation for 'VÄRD'

--- src/app/components/volunteer/volunteer.component.ts
+++ src/app/components/volunteer/volunteer.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VolunteerComponent implements OnInit {
 
+  isApplicationOpen: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkTime();
   }
 
+  checkTime() {
+    const now = new Date();
+
+    // Stäng ansökan 2:a september, kl. 23:59:59
+    const closeApplicationDate = new Date(now.getFullYear(), 8, 2, 23, 59, 59, 999); // OBS! Månaden är 0-indexerad, så 8 = september
+
+    if (now < closeApplicationDate) {
+      this.isApplicationOpen = true;
+
+      const timeUntilHide = closeApplicationDate.getTime() - now.getTime();
+      setTimeout(() => {
+        this.isApplicationOpen = false;
+      }, timeUntilHide);
+    } else {
+      this.isApplicationOpen = false;
+    }
+  }
 }
