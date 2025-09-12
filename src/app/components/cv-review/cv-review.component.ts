a67d25616bb8a5e7911d4b0224a471a7cf105054
Anja <anja.01.sandgren@gmail.com>
2025-09-13 00:40:00 +0200

    Added when CV-review booking should open automatically

--- src/app/components/cv-review/cv-review.component.ts
+++ src/app/components/cv-review/cv-review.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./cv-review.component.scss']
 })
 export class CvReviewComponent {
+  
+  isApplicationOpen: boolean = false;
+
   constructor() { }
+
+  ngOnInit(): void {
+    this.checkTime();
+  }
+
+    checkTime() {
+    const now = new Date();
+
+    // Open the 17th of September 2025, 12:00:00
+    const openApplicationDate = new Date(2025, 8, 7, 12, 0, 0); // 0-index months so 8 = September
+    const closeApplicationDate = new Date(2025, 8, 24, 10, 0, 0);
+    
+    if (now > closeApplicationDate) {
+      this.isApplicationOpen = false;
+    } else if (now > openApplicationDate) {
+      this.isApplicationOpen = true;
+    } else {
+      this.isApplicationOpen = false;
+    }
+  }
 }
