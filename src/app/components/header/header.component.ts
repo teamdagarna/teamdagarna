a7f85efced10255863f73e2790a67c0371cb6d32
Fredrik Sahlberg <fredriksahlberg@MacBook-Air-M2-2.local>
2026-08-22 13:01:11 +0200

    Hamburgarmeny försvinner vid klick på menyval

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,13 +13,18 @@ export class HeaderComponent implements OnInit {
   isMenuActive: boolean = false;  
   constructor(public auth: AuthService, public router: Router) {
     this.auth.user$.subscribe(user => this.user = user)
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isMenuActive = false;
+    });
   }
   ngOnInit() {}
   toggleMenu() {
     this.isMenuActive = !this.isMenuActive;
   }
   closeMenu() {
-    console.log('closeMenu called');
     this.isMenuActive = false;
   }
 }
--- src/assets/js/bulma-responsive.js
+++ src/assets/js/bulma-responsive.js
@@ -1,47 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+//   // Get all "navbar-burger" elements
+//   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  var $navItems = Array.prototype.slice.call(document.querySelectorAll('.mobileLink'), 0);
+//   var $navItems = Array.prototype.slice.call(document.querySelectorAll('.mobileLink'), 0);
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(function ($el) {
+//       $el.addEventListener('click', function () {
 
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+//         // Get the target from the "data-target" attribute
+//         var target = $el.dataset.target;
+//         var $target = document.getElementById(target);
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
 
-      });
-    });
-  }
-  // Check if there are any navbar items with class .mobileLink
-  if($navItems.length > 0) {
+//       });
+//     });
+//   }
+//   // Check if there are any navbar items with class .mobileLink
+//   if($navItems.length > 0) {
 
-    // Add a click event on each of them
-    $navItems.forEach(function ($e1) {
-      $e1.addEventListener('click', function() {
-        // Get the target from the "data-target" attribute
-        var target = "navbarExampleTransparentExample"
-        var $target = document.getElementById(target);
+//     // Add a click event on each of them
+//     $navItems.forEach(function ($e1) {
+//       $e1.addEventListener('click', function() {
+//         // Get the target from the "data-target" attribute
+//         var target = "navbarExampleTransparentExample"
+//         var $target = document.getElementById(target);
 
-        var target2 = "navbarBurger"
-        var $target2 = document.getElementById(target2);
+//         var target2 = "navbarBurger"
+//         var $target2 = document.getElementById(target2);
 
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $target.classList.toggle('is-active');
-        $target2.classList.toggle('is-active');
-      })
-    })
-  }
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $target.classList.toggle('is-active');
+//         $target2.classList.toggle('is-active');
+//       })
+//     })
+//   }
 
-});
+// });
